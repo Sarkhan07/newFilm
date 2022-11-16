3b261fb4358d2c919c338ad7385b7b6e704ac124
Sarkhan <abdullayevsarxan92@gmail.com>
2022-11-17 00:18:21 +0400

    feat: adding list of movies with js

--- js/script.js
+++ js/script.js
@@ -16,11 +16,36 @@
 
 const movieDB = {
     movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+        'Логан',
+        'Лига справедливости',
+        'Ла-ла лэнд',
+        'Одержимость',
+        'Скотт Пилигрим против...',
+    ],
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    bgImage = document.querySelector('.promo__bg'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    genre = bgImage.querySelector('.promo__genre');
+
+// adv.remove(); if it was one not array it will work
+adv.forEach((ad) => {
+    ad.remove();
+});
+
+genre.textContent = 'драма';
+bgImage.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = ''; // innerHTML есть только у queryselector
+
+movieDB.movies.sort();
+// console.log(bgImage.innerHTML) получать элементы со страницы
+
+movieDB.movies.forEach((movie, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item"> ${i + 1}) ${movie}
+          <div class="delete"></div>
+    </li>
+    `;
+});
